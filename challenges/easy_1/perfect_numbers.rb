cd440b139c9f96e2c143f67b5f706cc20f49c392
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-12-31 12:42:47 -0500

    updating perfect_numbers.rb

--- challenges/easy_1/perfect_numbers.rb
+++ challenges/easy_1/perfect_numbers.rb
@@ -1,11 +1,8 @@
 # perfect_numbers.rb
+require 'prime'
+
 class PerfectNumber
-  def self.classify(input_num)
-    raise StandardError if input_num < 0
-
-    sum_of_factors = (1...input_num).select { |n| (input_num % n).zero? }
-    sum_of_factors = sum_of_factors.reduce(:+)
-
+  def self.find_category(input_num, sum_of_factors)
     if input_num == sum_of_factors
       'perfect'
     elsif input_num > sum_of_factors
@@ -13,5 +10,16 @@ class PerfectNumber
     elsif input_num < sum_of_factors
       'abundant'
     end
+  end
+
+  def self.classify(input_num)
+    raise StandardError if input_num < 0
+    return 'deficient' if Prime.prime?(input_num)
+
+    sum_of_factors = (1...input_num).select do |n|
+      (input_num % n).zero?
+    end.reduce(:+)
+
+    find_category(input_num, sum_of_factors)
   end
 end
