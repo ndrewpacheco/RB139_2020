1c68febf6635a2866d42e5a2e5079bca58020b77
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-01-02 02:32:45 -0500

    refactoring secrethandshake

--- challenges/medium_1/secret_handshake.rb
+++ challenges/medium_1/secret_handshake.rb
@@ -1,20 +1,63 @@
-# Secret Handshake
-# Write a program that will take a decimal number, and convert it to the appropriate sequence of events for a secret handshake.
+# secret_handshake.rb
+class SecretHandshake
+  COMMANDS = ['wink', 'double blink', 'close your eyes', 'jump'].freeze
 
-# There are 10 types of people in the world: Those who understand binary, and those who don't. You and your fellow cohort of those in the "know" when it comes to binary decide to come up with a secret "handshake".
+  def initialize(num)
+    @num = num.to_i
+  end
 
-# Copy Code
-# 1 = wink
-# 10 = double blink
-# 100 = close your eyes
-# 1000 = jump
+  def commands
+    @num = format('%b', @num).chars.reverse
+    result_arr = []
+
+    @num.each_with_index do |digit, idx|
+      result_arr << COMMANDS[idx] if digit == '1'
+    end
+
+    if @num.size > 4
+      result_arr.pop
+      result_arr.reverse!
+    end
+
+    result_arr
 
 
-# 10000 = Reverse the order of the operations in the secret handshake.
-# Copy Code
-# handshake = SecretHandshake.new 9
-# handshake.commands # => ["wink","jump"]
 
-# handshake = SecretHandshake.new "11001"
-# handshake.commands # => ["jump","wink"]
-# The program should consider strings specifying an invalid binary as the value 0.
+  end
+end
+
+
+# handshake = SecretHandshake.new(1)
+# puts handshake.commands
+
+# handshake = SecretHandshake.new(2)
+# puts handshake.commands
+
+
+  # def test_handshake_10_to_double_blink
+  #   #skip
+
+
+  # def test_handshake_100_to_close_your_eyes
+  #  # skip
+  #   handshake = SecretHandshake.new(4)
+  #   assert_equal ['close your eyes'], handshake.commands
+  # end
+
+  # def test_handshake_1000_to_jump
+  #   #skip
+  #   handshake = SecretHandshake.new(8)
+  #   assert_equal ['jump'], handshake.commands
+  # end
+
+  # def test_handshake_11_to_wink_and_double_blink
+  #  # skip
+  #   handshake = SecretHandshake.new(3)
+  #   assert_equal ['wink', 'double blink'], handshake.commands
+  # end
+
+  # def test_handshake_10011_to_double_blink_and_wink
+  #   #skip
+  #   handshake = SecretHandshake.new(19)
+  #   assert_equal ['double blink', 'wink'], handshake.commands
+  # end
