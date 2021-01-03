c80404be71aabf452006a28fd0707a23b6e8ac79
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-01-03 17:22:00 -0500

    fin luhn

--- challenges/medium_1/luhn.rb
+++ challenges/medium_1/luhn.rb
@@ -1,28 +1,37 @@
-# Luhn Algorithm
-# The Luhn formula is a simple checksum formula used to validate a variety of identification numbers, such as credit card numbers and Canadian Social Insurance Numbers.
+# luhn.rb
+class Luhn
+  attr_accessor :num
+  def initialize(num)
+    @num = num
+  end
 
-# The formula verifies a number against its included check digit, which is usually appended to a partial number to generate the full number. This number must pass the following test:
+  def self.create(num)
+    luhn = Luhn.new(num)
+    luhn.num *= 10
+    loop do
+      break if luhn.valid?
+      luhn.numx += 1
+    end
+    luhn.num
+  end
 
-# Counting from rightmost digit (which is the check digit) and moving left, double the value of every second digit.
-# For any digits that thus become 10 or more, subtract 9 from the result.
-# 1111 becomes 2121.
-# 8763 becomes 7733 (from 2×6=12 → 12-9=3 and 2×8=16 → 16-9=7).
-# Add all these digits together.
-# 1111 becomes 2121 sums as 2+1+2+1 to give a checksum of 6.
-# 8763 becomes 7733, and 7+7+3+3 is 20.
-# If the total (the checksum) ends in 0 (put another way, if the total modulo 10 is congruent to 0), then the number is valid according to the Luhn formula; else it is not valid. So, 1111 is not valid (as shown above, it comes out to 6), while 8763 is valid (as shown above, it comes out to 20).
+  def addends
+    @num.to_s.chars.reverse.map.with_index do |digit, idx|
+      digit = digit.to_i
+      if idx.odd?
+        digit *= 2
+        digit >= 10 ? digit - 9 : digit
+      else
+        digit
+      end
+    end.reverse
+  end
 
-# Write a program that, given a number
+  def checksum
+    addends.reduce(:+)
+  end
 
-# Can check if it is valid per the Luhn formula. This should treat, for example, "2323 2005 7766 3554" as valid.
-# Can return the checksum, or the remainder from using the Luhn method.
-# Can add a check digit to make the number valid per the Luhn formula and return the original number plus that digit. This should give "2323 2005 7766 3554" in response to "2323 2005 7766 355".
-# About Checksums:
-
-# A checksum has to do with error-detection. There are a number of different ways in which a checksum could be calculated.
-
-# When transmitting data, you might also send along a checksum that says how many bytes of data are being sent. That means that when the data arrives on the other side, you can count the bytes and compare it to the checksum. If these are different, then the data has been garbled in transmission.
-
-# In the Luhn problem the final digit acts as a sanity check for all the prior digits. Running those prior digits through a particular algorithm should give you that final digit.
-
-# It doesn't actually tell you if it's a real credit card number, only that it's a plausible one. It's the same thing with the bytes that get transmitted -- you could have the right number of bytes and still have a garbled message. So checksums are a simple sanity-check, not a real in-depth verification of the authenticity of some data. It's often a cheap first pass, and can be used to quickly discard obviously invalid things.
+  def valid?
+    (checksum % 10).zero?
+  end
+end
--- challenges/medium_1/secret_handshake.rb
+++ challenges/medium_1/secret_handshake.rb
@@ -1,35 +1,16 @@
 # secret_handshake.rb
-ONE = "1"
-ZERO = "0"
+ONE = '1'.freeze
+ZERO = '0'.freeze
 COMMANDS = ['wink', 'double blink', 'close your eyes', 'jump'].freeze
-
+##
 class SecretHandshake
   def initialize(num)
     @num = num.to_i
+    @binary_num = @num
   end
 
   def commands
-    binary_arr = []
-    binary_num = @num
     commands_arr = []
-
-    # first determine size of binary arr
-    counter = 0
-    loop do
-      break if @num / 2**counter == 0
-      counter += 1
-      binary_arr << nil
-    end
-
-    binary_arr.map!.with_index do |item, idx|
-      counter -= 1
-      if binary_num / 2**counter == 1
-        binary_num -= 2**counter
-        ONE
-      else
-        ZERO
-      end
-    end
 
     binary_arr.reverse.each_with_index do |digit, idx|
       commands_arr << COMMANDS[idx] if digit == '1'
@@ -40,58 +21,49 @@ class SecretHandshake
       commands_arr.reverse!
     end
     commands_arr
+
+    #   my first answer without manually converting integer to binary:
+    #   @num = format('%b', @num).chars.reverse
+    #   result_arr = []
+
+    #   @num.each_with_index do |digit, idx|
+    #     result_arr << COMMANDS[idx] if digit == '1'
+    #   end
+    #   if @num.size > 4
+    #     result_arr.reject!(&:nil?)
+    #     result_arr.reverse!
+    #   end
+    #   result_arr
+    #   end
   end
 
-  # my first answer without manually converting integer to binary:
+  private
 
-  #   @num = format('%b', @num).chars.reverse
-  #   result_arr = []
+  def find_size
+    counter = 0
+    loop do
+      break if (@num / 2**counter).zero?
+      counter += 1
+    end
+    counter
+  end
 
-  #   @num.each_with_index do |digit, idx|
-  #     result_arr << COMMANDS[idx] if digit == '1'
-  #   end
+  def fill(binary_arr)
+    counter = find_size
+    while counter > 0
+      counter -= 1
+      if @binary_num / 2**counter == 1
+        @binary_num -= 2**counter
+        binary_arr << ONE
+      else
+        binary_arr << ZERO
+      end
+    end
+  end
 
-  #   if @num.size > 4
-  #     result_arr.reject!(&:nil?)
-  #     result_arr.reverse!
-  #   end
-
-  #   result_arr
-  # end
+  def binary_arr
+    binary_arr = []
+    fill(binary_arr)
+    binary_arr
+  end
 end
-
-
-handshake = SecretHandshake.new(19)
-p handshake.commands
-
-handshake = SecretHandshake.new(31)
-p handshake.commands
-
-
-  # def test_handshake_10_to_double_blink
-  #   #skip
-
-
-  # def test_handshake_100_to_close_your_eyes
-  #  # skip
-  #   handshake = SecretHandshake.new(4)
-  #   assert_equal ['close your eyes'], handshake.commands
-  # end
-
-  # def test_handshake_1000_to_jump
-  #   #skip
-  #   handshake = SecretHandshake.new(8)
-  #   assert_equal ['jump'], handshake.commands
-  # end
-
-  # def test_handshake_11_to_wink_and_double_blink
-  #  # skip
-  #   handshake = SecretHandshake.new(3)
-  #   assert_equal ['wink', 'double blink'], handshake.commands
-  # end
-
-  # def test_handshake_10011_to_double_blink_and_wink
-  #   #skip
-  #   handshake = SecretHandshake.new(19)
-  #   assert_equal ['double blink', 'wink'], handshake.commands
-  # end
