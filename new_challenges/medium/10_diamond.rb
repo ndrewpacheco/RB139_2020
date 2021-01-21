3edc0124a39262bbe2124d2844f63218e8278e3d
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-01-21 16:18:43 -0500

    fin 10 diamond

---
+++ new_challenges/medium/10_diamond.rb
@@ -0,0 +1,42 @@
+class Diamond
+  WHITESPACE = ' '
+
+  def self.make_diamond(letter)
+    ascending_alphabet = descending_alphabet(letter).reverse
+    ascending_alphabet.shift
+    (descending_alphabet(letter) + ascending_alphabet).join
+  end
+
+  def self.descending_alphabet(letter)
+    result = 'A'.upto(letter).to_a
+    margin_length = result.size
+    mid_length = 1
+    configure_whitespace(result, margin_length, mid_length)
+  end
+
+  def self.configure_whitespace(result, margin_length, mid_length)
+    result.map do |char|
+      margin_length -= 1
+      margin_whitespace = WHITESPACE * margin_length
+      mid_whitespace = WHITESPACE * mid_length
+
+      next make_line(char, margin_whitespace) if char == 'A'
+      mid_length += 2
+      make_line(char, margin_whitespace, mid_whitespace)
+    end
+  end
+
+  def self.make_line(char, margin_whitespace, mid_whitespace = '')
+    if char == 'A'
+      margin_whitespace + char + margin_whitespace + "\n"
+    else
+      margin_whitespace +
+        char +
+        mid_whitespace +
+        char +
+        margin_whitespace + "\n"
+    end
+  end
+end
+
+p Diamond.make_diamond('C')
